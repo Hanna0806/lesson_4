8d5000ecf27e7efaae6e692d3e3b24475541294a
Hanna0806 <anna.gal0806@gmail.com>
2025-03-07 20:46:49 +0300

    feat: add function to reverse strings

---
+++ main.js
@@ -0,0 +1,6 @@
+// 4) Развернуть строку в обратном направлении при помощи методов массивов:
+
+function reverseStr(str) {
+    return str.split('').reverse().join('')
+}
+console.log(reverseStr('Ivan'));
