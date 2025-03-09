30b04a3dff2a0adfd5098b0c8a9598fd12cab50c
Hanna0806 <anna.gal0806@gmail.com>
2025-03-09 13:52:08 +0300

    feat: add function selectFromInterval

--- main.js
+++ main.js
@@ -37,4 +37,52 @@ console.log('ob: ', ob.b.c)
 console.log('obCopy: ', obCopy.a)
 console.log('obCopy: ', obCopy.b.c)
 
+// 2) Создать функцию selectFromInterval (arr, a, b) {...}
+// принимает 3 параметра: arr, a, b
+// arr - массив чисел
+// a, b - целые числа
+
+// Функция должна вернуть новый массив, который включает в себя элементы массива arr вошедшие в интервал (включительно) между  a и b 
+// (или  b и a, в зависимости от того, что больше a или b). Возвращаемый массив должен быть отсортирован по возрастанию.
+
+// если arr - не массив, то кидаем ошибку "first parameter must be an array!";
+// если arr не полностью состоит из чисел (есть другие типы),  то кидаем ошибку "There are not only numbers in the array!";
+// если a и/или b не целое число, то кидаем ошибку "incorrect input parameters!".
+
+function selectFromInterval(arr, a, b) {
+    if (!Array.isArray(arr)) {
+        throw new Error('first parameter must be an array!')
+    }
+
+    if (!arr.every(item => typeof item === 'number')) {
+        throw new Error('There are not only numbers in the array!')
+    }
+
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        throw new Error('incorrect input parameters!')
+    }
+
+    const minNum = Math.min(a, b);
+    const maxNum = Math.max(a, b);
+
+    const filteredArr = arr.filter(item => item >= minNum && item <= maxNum);
+    const sortedArr = filteredArr.sort((num1, num2) => num1 - num2);
+
+    return sortedArr;
+}
+
+// const arr1 = selectFromInterval(7, 5, 9);
+// const arr2 = selectFromInterval([4, 8, 11, '66', 94, 7, '32'], 11, 94);
+// const arr3 = selectFromInterval([4, 8, 11, 66, 94, 7, 32], 11.5, 94);
+
+const arr4 = selectFromInterval([4, 8, 11, 66, 94, 7, 32], 11, 66);
+console.log(arr4)
+
+const arr5 = selectFromInterval([4, 8, 11, 66, 94, 7, 32], 11, 66);
+console.log(arr5)
+
+
+
+
+
 
